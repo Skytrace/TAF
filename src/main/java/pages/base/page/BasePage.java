8980f96ca29989b0a57874900a5b34e6471591b7
Andrey <andre.sobolev@gmail.com>
2016-06-10 13:44:30 +0300

    replace methods

--- src/main/java/pages/base/page/BasePage.java
+++ src/main/java/pages/base/page/BasePage.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 public class BasePage {
     private static ThreadLocal<WebDriver> web = new ThreadLocal<WebDriver>();
 
-    public WebDriver web() {
-        return web.get();
-    }
-
     public BasePage() {}
 
     public BasePage(WebDriver web) {
         this.web.set(web);
+    }
+
+    public WebDriver web() {
+        return web.get();
     }
 }
