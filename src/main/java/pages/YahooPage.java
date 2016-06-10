15322eea3d75ae81a3f6eb1f72246bc2839f9d10
Andrey <andre.sobolev@gmail.com>
2016-06-10 15:52:24 +0300

    click by fake element

--- src/main/java/pages/YahooPage.java
+++ src/main/java/pages/YahooPage.java
@@ -1,10 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import pages.base.page.BasePage;
 
 public class YahooPage extends BasePage {
 
     public void openYahooMainPage() {
         web().get("http://yahoo.com");
+    }
+
+    public void clickBySpecificElement() {
+        web().findElement(By.id("test")).click();
     }
 }
